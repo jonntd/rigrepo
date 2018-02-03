3d320e5f8cae7503e331d829fdba88766f84e5eb
unknown <mattschiller@gmail.com>
2018-02-02 19:46:34 -0800

    Adding test files

---
+++ tests/yankWireToSkinCluster.py
@@ -0,0 +1,18 @@
+import rigrepo.libs.shape
+import rigrepo.libs.weights
+import maya.cmds as mc
+reload(rigrepo.libs.weights)
+reload(rigrepo.libs.shape)
+
+base = 'pCylinder1_noDeformation'
+target = 'pCylinder1_wireDeformed'
+sc = 'skinCluster2'
+
+for joint in mc.ls('joint?'):
+    mc.move( 1, 0, 0, joint, r=1) 
+    deltas = rigrepo.libs.shape.getDeltas(base, target)
+    #print(deltas)
+    mc.move( -1, 0, 0, joint, r=1) 
+    rigrepo.libs.weights.setWeight(sc, deltas, map=joint) 
+    
+mc.skinPercent(sc, mc.deformer(sc, q=1, geometry=1)[0], normalize=1) # in case of floating point precision 
