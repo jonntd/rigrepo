6b7106d90416c9ce374f79d9e385d21b7ca42652
Walter Yoder <walteryoder@gmail.com>
2018-02-08 01:48:41 -0800

    updated the test code to make sure it testing the latest updates.

--- tests/bipedPartNodes.py
+++ tests/bipedPartNodes.py
@@ -1,9 +1,17 @@
-import pubs.ui.mainWindow; 
-reload(pubs.ui.mainWindow)
-import rigrepo.templates.biped.biped as bt
-reload(bt)
-import rigrepo.libs.attribute
-reload(rigrepo.libs.attribute)
-graph = bt.Biped("elsa")
+import pubs.ui.mainWindow
+import rigrepo.templates.biped.biped as biped
+import rigrepo.libs.data.joint_data
+reload(biped)
+import os
 
-pubs.ui.mainWindow.launch(graph=graph)
+buildPath = os.path.join(os.path.dirname(biped.__file__),"build")
+import maya.cmds as mc
+mc.file(os.path.join(buildPath,"skeleton.ma"), o=True,f=True)
+data = rigrepo.libs.data.joint_data.JointData()
+data.read(os.path.join(buildPath,"joint_positions.data"))
+data.applyData(mc.ls(type="joint"))
+
+# the right arm is all messed up and we have to fix it. Also, you can't run nodes more than once
+# in the same scene at th moment. We will have to fix this.
+matt_graph = biped.Biped('matt')
+pubs.ui.mainWindow.launch(graph=matt_graph)
