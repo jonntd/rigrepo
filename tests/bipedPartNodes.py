e39c23f9e923a06a89f6680ba3d1ac8ea879142d
Walter Yoder <walteryoder@gmail.com>
2018-02-14 08:56:21 -0800

    added parts proper reload sequence along with reloading the UI.

--- tests/bipedPartNodes.py
+++ tests/bipedPartNodes.py
@@ -1,5 +1,23 @@
 import pubs.ui.mainWindow
-import rigrepo.templates.biped.biped as biped
+reload(pubs.ui.mainWindow)
+# reload parts
+import rigrepo.parts.part
+reload(rigrepo.parts.part)
+import rigrepo.parts.limb
+reload(rigrepo.parts.limb)
+import rigrepo.parts.arm
+reload(rigrepo.parts.arm)
+import rigrepo.parts.spine
+reload(rigrepo.parts.spine)
+import rigrepo.parts.neck
+reload(rigrepo.parts.neck)
+import rigrepo.parts.blink
+reload(rigrepo.parts.blink)
+#reload templates
+import rigrepo.templates.base.base
+reload(rigrepo.templates.base.base)
+import rigrepo.templates.biped.biped
+reload(rigrepo.templates.biped.biped)
 import rigrepo.libs.data.joint_data
 import os
 import maya.cmds as mc
@@ -16,5 +34,5 @@ data.applyData(joints)
 
 # the right arm is all messed up and we have to fix it. Also, you can't run nodes more than once
 # in the same scene at th moment. We will have to fix this.
-matt_graph = biped.Biped('matt')
+matt_graph = rigrepo.templates.biped.biped.Biped('matt')
 pubs.ui.mainWindow.launch(graph=matt_graph)
