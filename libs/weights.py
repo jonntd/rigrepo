786aa5337ef24d9fad07d99626a3177016a47f92
Walter Yoder <walteryoder@gmail.com>
2018-03-31 04:18:21 -0700

    added import and exporting of weights. This is a first pass of being able to export maps per mesh per deformer. I am pushing it so we can talk about how we want to handle it. We could switch to a one file solution....

--- libs/weights.py
+++ libs/weights.py
@@ -1,5 +1,7 @@
 import maya.cmds as mc
-import 
+import rigrepo.libs.common
+import os
+import maya.api.OpenMaya as om
 def setWeights(node, weights, map=None): 
     '''
     Sets weights for specified deformers.
@@ -62,3 +64,127 @@ def getWeights(node, map=None):
         for w in weights: 
             pntIndex,value = weight
             mc.setAttr(node+'.wl[0].w['+pntIndex+']', value) 
+
+
+def exportWeights(geometry, deformer, directory):
+    '''
+    This will export weights for the given deformer into the given directory.
+    If the directory doesn't exists. It will create the full path. 
+
+    .. example::
+        exportWeights("body_geo",
+            "body_geo_skinCluster",
+            "shows/template/collections/rigrepo/templates/biped/rig/build/base/skin_wts")
+
+        exportWeights(["body_geo",eye_l_geo],
+            "cluster1",
+            "shows/template/collections/rigrepo/templates/biped/rig/build/base/cluster_wts")
+
+    .. note:: 
+        This is setup to export to only one directory. So typically you will only
+        want to export deformers of the same type to the same directory
+    
+    :param geometry: The geometry(s) you wish to export the weights for.
+    :type geometry: str | list
+    
+    :param deformer: This is the deforemr(s) you wish to export
+    :type deformer: str | list
+
+    :param directory: The directory you wish to store the deformers passed. 
+    :type directory: str
+    '''
+    # importing pubs.ui so we can get the maya main window widget.
+    import pubs.ui
+    from PySide2 import QtWidgets
+    # make sure that we have a list for the rest of the function to work properly
+    geoList = rigrepo.libs.common.toList(geometry)
+    deformerList = rigrepo.libs.common.toList(deformer)
+    
+    # check to make sure the directory exists. If it doesn't we will create the directory.
+    if not os.path.isdir(directory):
+        msg = 'The following directory does not exist: {}\n\
+        Do you wish to create it so you can export the deformers?'.format(directory)
+        reply = QtWidgets.QMessageBox.question(pubs.ui.getMayaWindow(),
+                                                'Create directory dialog',
+                                                msg,
+                                                QtWidgets.QMessageBox.Yes, 
+                                                QtWidgets.QMessageBox.No)
+        if reply == QtWidgets.QMessageBox.Yes:
+            os.makedirs(directory, 755)
+        else:
+            print "since there is not a directory, we cannot export the deformers."
+            return
+        
+    
+    # here we will loop through all of the the deformers and geometry and export the files
+    # to the given directory.
+    for deformer in deformerList:
+        for geo in geoList:
+            # adding the geometry to a selection list so I can extend to the shape
+            # reliably with out any issues.
+            selList = om.MSelectionList()
+            selList.add(geo)
+            dagPath = selList.getDagPath(0)
+            dagPath.extendToShape()
+            # once we have the correct shape. Now we will check it against the other shapes 
+            # that the given deformer is influencing.
+            shapes = mc.deformer(deformer, q=True,g=True)
+            geoShape = dagPath.partialPathName()
+            
+            # get the difference between the deformer and the deformers influenced by
+            if geoShape in shapes:
+                skipGeo = ";".join(list(set(shapes).difference(set([geoShape]))))
+
+            # this is where we will export the weights.
+            mc.deformerWeights("{}__{}.xml".format(geo, deformer), 
+                                skip=skipGeo, 
+                                export=True, 
+                                deformer=deformer,
+                                path=directory)
+
+def importWeights(geometry, deformer, filepath):
+    '''
+    This will import weights for the given deformer into the given directory.
+
+    .. example::
+        importWeights("body_geo",
+            "cluster1",
+            "shows/template/collections/rigrepo/templates/biped/rig/build/base/cluster_wts/body_geo__cluster1.xml)
+
+    .. note::
+        This is setup to import only one file at a time. We should write something that imports
+        multiple files in a directory
+    
+    :param geometry: The geometry(s) you wish to import the weights for.
+    :type geometry: str
+    
+    :param deformer: This is the deforemr(s) you wish to import weights onto
+    :type deformer: str
+
+    :param filepath: The filepath you want to use for the weights.
+    :type filepath: str
+    '''
+    if not os.path.isfile(filepath):
+        raise RuntimeError("{} is not an existing filepath.".format(filepath))
+
+    # split up the path so we can pass the file name and directory in seperately
+    filename = os.path.basename(filepath)
+    directory = os.path.dirname(filepath)
+
+    # adding the geometry to a selection list so I can extend to the shape
+    # reliably with out any issues.
+    selList = om.MSelectionList()
+    selList.add(geometry)
+    dagPath = selList.getDagPath(0)
+    dagPath.extendToShape()
+    # once we have the correct shape. Now we will check it against the other shapes 
+    # that the given deformer is influencing.
+    shapes = mc.deformer(deformer, q=True,g=True)
+    geoShape = dagPath.partialPathName()
+    
+    # get the difference between the deformer and the deformers influenced by
+    if geoShape in shapes:
+        skipGeo = ";".join(list(set(shapes).difference(set([geoShape]))))
+
+    #import the weights for the given deformer and filepath
+    mc.deformerWeights(filename, im=True, deformer=deformer, skip=skipGeo, path=directory)
